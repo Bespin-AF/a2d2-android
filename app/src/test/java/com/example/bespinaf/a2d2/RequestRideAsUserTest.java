4b619f9272dd6a1ea8ac5c778bc56d6459c10d68
joshua green <jgreen@joshuas-MacBook-Pro.local>
2018-11-14 11:39:45 -0600

    Test commit for the home screen

--- app/src/test/java/com/example/bespinaf/a2d2/RequestRideAsUserTest.java
+++ app/src/test/java/com/example/bespinaf/a2d2/RequestRideAsUserTest.java
@@ -1,5 +1,14 @@
+package com.example.bespinaf.a2d2;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class RequestRideAsUserTest {
+    @Test
+
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
 
 }
