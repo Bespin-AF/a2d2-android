82ab3865074662bbfe02059e82d9c69e9eb8e2dd
Scott Yamamoto <scottyamamoto@Scotts-MacBook-Pro.local>
2018-11-15 11:37:34 -0600

    Added test for button clicking

--- app/src/androidTest/java/com/example/bespinaf/a2d2/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/bespinaf/a2d2/ExampleInstrumentedTest.java
@@ -2,10 +2,21 @@ package com.example.bespinaf.a2d2;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.espresso.intent.matcher.IntentMatchers;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.rule.ActivityTestRule;
+import android.widget.Button;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.Assert.*;
 
@@ -16,11 +27,53 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivity = new ActivityTestRule<>(MainActivity.class);
+
+    public MainActivity activity;
+
+    @Before
+    public void setUp(){
+        activity = mainActivity.getActivity();
+    }
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.bespinaf.a2d2", appContext.getPackageName());
+    }
+
+    @Test
+    public void hasButton(){
+        final Button btn = activity.findViewById(R.id.button);
+
+        activity.runOnUiThread(new Runnable(){
+            @Override
+            public void run(){
+                btn.performClick();
+                System.out.println("Button click successful.");
+            }
+        });
+    }
+
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
     }
 }
