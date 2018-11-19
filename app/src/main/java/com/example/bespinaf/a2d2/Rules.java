43a4f8ed89a77c7135a26b3ba876c2c25016ce82
joshua green <jgreen@joshuas-MacBook-Pro.local>
2018-11-19 11:25:51 -0600

    User grants permissions story: Added the permissions pop-up

--- app/src/main/java/com/example/bespinaf/a2d2/Rules.java
+++ app/src/main/java/com/example/bespinaf/a2d2/Rules.java
@@ -2,37 +2,68 @@ package com.example.bespinaf.a2d2;
 
 import com.example.bespinaf.a2d2.utilities.Permissions;
 
+import android.annotation.TargetApi;
 import android.app.DownloadManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.widget.Toast;
+
+import java.util.UUID;
 
 
-public class Rules extends AppCompatActivity {
+public class Rules extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private final int MY_PERMISSIONS_REQUEST_LOCATION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
     }
-
-    public void btnRulesAgree_Clicked(View sender){
-        if(!Permissions.isLocationPermissionGranted(this)){
-            return;
+    @TargetApi(Build.VERSION_CODES.M)
+    public void btnRulesAgree_Clicked(View sender)
+    {
+        if(!Permissions.isLocationPermissionGranted(this))
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},0);
         }
-
-        Intent intent = new Intent(this, RequestRide.class);
-        startActivity(intent);
+        else
+        {
+            Intent intent = new Intent(this, RequestRide.class);
+            startActivity(intent);
+        }
     }
-    public boolean checkLocationPermission(){
-        boolean isLocationEnabled = Permissions.isLocationPermissionGranted(this);
 
-
-        return false;
-    }
-    public void requestLocationPermission(){
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        if (grantResults.length == 0 || grantResults[0] == PackageManager.PERMISSION_DENIED)
+        {
+            if(requestCode == MY_PERMISSIONS_REQUEST_LOCATION)
+            {
+                Toast.makeText(this, R.string.error_LocationPermissionDenied, Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                Toast.makeText(this, R.string.error_DefaultError, Toast.LENGTH_LONG).show();
+            }
+        }
+        else
+        {
+            Intent intent = new Intent(this, RequestRide.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/bespinaf/a2d2/utilities/Permissions.java
+++ app/src/main/java/com/example/bespinaf/a2d2/utilities/Permissions.java
@@ -1,55 +1,21 @@
 package com.example.bespinaf.a2d2.utilities;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 
-public class Permissions {
-    public static boolean getFalse(){
-        return false;
+import com.example.bespinaf.a2d2.R;
+
+public class Permissions
+{
+    public static boolean isLocationPermissionGranted(Context context)
+    {
+        return(ContextCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED);
     }
-
-    public static boolean isLocationPermissionGranted(Context context){
-        /*if (ContextCompat.checkSelfPermission(context,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-                new AlertDialog.Builder(this)
-                        .setTitle(R.string.title_location_permission)
-                        .setMessage(R.string.text_location_permission)
-                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                //Prompt the user once explanation has been shown
-                                ActivityCompat.requestPermissions(MainActivity.this,
-                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                                        MY_PERMISSIONS_REQUEST_LOCATION);
-                            }
-                        })
-                        .create()
-                        .show();
-
-
-            } else {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_LOCATION);
-            }
-            return false;
-        } else {
-            return true;
-        }*/
-        return true;
-    }
-
 }
