4ad31db1a0478114474b219af8c5b0353bdeae4e
joshua green <jgreen@joshuas-MacBook-Pro.local>
2019-01-25 15:05:32 -0600

    Fixing thing

--- app/src/main/java/com/example/bespinaf/a2d2/MainActivity.java
+++ app/src/main/java/com/example/bespinaf/a2d2/MainActivity.java
@@ -1,26 +1,48 @@
 package com.example.bespinaf.a2d2;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.button.MaterialButton;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button_navigate_to_rules)
+    MaterialButton buttonNavigateToRules;
+    @BindView(R.id.button_main_driver_login)
+    MaterialButton buttonMainDriverLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     /**
      * //Open the rules page
+     *
      * @param view the Request Ride Navigation Button
      */
+    @OnClick(R.id.button_navigate_to_rules)
     public void openRules(View view) {
         Intent intent = new Intent(this, Rules.class);
+        startActivity(intent);
+    }
+
+    /**
+     * //Open the driver login page
+     *
+     * @param view the Request Ride Navigation Button
+     */
+    @OnClick(R.id.button_main_driver_login)
+    public void openDriverLogin(View view) {
+        Intent intent = new Intent(this, DriverLogin.class);
         startActivity(intent);
     }
 }
