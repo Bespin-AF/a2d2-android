8cc7870b23c68f3eab0ddcec70ddf86dda35100c
Chance Crawford <chancecrawford@gmail.com>
2019-06-06 12:34:43 -0500

    Writing test for progress bar implementation.

--- app/src/androidTest/java/com/example/bespinaf/a2d2/loading_bar_test.java
+++ app/src/androidTest/java/com/example/bespinaf/a2d2/loading_bar_test.java
@@ -3,35 +3,53 @@ package com.example.bespinaf.a2d2;
 import android.app.Instrumentation;
 import android.support.test.rule.ActivityTestRule;
 
+import com.example.bespinaf.a2d2.controllers.Driver_Login;
+import com.example.bespinaf.a2d2.controllers.Driver_RideRequestList;
 import com.example.bespinaf.a2d2.controllers.MainActivity;
+import com.example.bespinaf.a2d2.controllers.Rider_RequestRide;
+import com.example.bespinaf.a2d2.controllers.Rider_Rules;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.RootMatchers.isDialog;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+
 public class loading_bar_test {
 
     @Rule
-    public ActivityTestRule<MainActivity> mMainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<Driver_Login> mProgressBarTest = new ActivityTestRule<>(Driver_Login.class);
 
-    private MainActivity mActivity;
+    private Driver_Login mDriverLoginActivity;
+    private Driver_RideRequestList mDriverRideRequestListActivity;
+    private Rider_RequestRide mRiderRequestRideActivity;
+    private Rider_Rules mRiderRulesActivity;
     private Instrumentation mInstrumentation;
-    private Instrumentation.ActivityMonitor mRulesMonitor;
-    private Instrumentation.ActivityMonitor mLoginMonitor;
 
     @Before
     public void setup(){
-        mActivity = mMainActivityTestRule.getActivity();
+        mInstrumentation = getInstrumentation();
     }
 
     @After
     public void teardown(){
-
+        mDriverLoginActivity = null;
+        mDriverRideRequestListActivity = null;
+        mRiderRequestRideActivity = null;
+        mRiderRulesActivity = null;
     }
 
     @Test
-    public void test(){
-        assert(true);
+    public void driverLoginTest(){
+        onView(withId(R.id.ride_request_progress_bar));
     }
 }
